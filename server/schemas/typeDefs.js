e3212a2d008af4e23ef7e99339ca23ff502cbe2a
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-29 14:53:21 -0400

    added template typeDef and Schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,54 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // typeDefs logic will go in the following section...
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    posts: [Post]!
+  }
 
+  type Post {
+    _id: ID
+    postText: String
+    postAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(postId: ID!): Post
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPost(postText: String!, postAuthor: String!): Post
+    addComment(
+      postId: ID!
+      commentText: String!
+      commentAuthor: String!
+    ): Post
+    removePost(postId: ID!): Post
+    removeComment(postId: ID!, commentId: ID!): Post
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
