6116a7435a966b486a1098e41cbf8077de46e920
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-29 15:39:38 -0400

    database file formatting

--- server/models/User.js
+++ server/models/User.js
@@ -9,42 +9,41 @@ const userSchema = new Schema({
 		required: true,
 		unique: true,
 		trim: true,
-	  },
-	  email: {
+	},
+	email: {
 		type: String,
 		required: true,
 		unique: true,
-		match: [/.+@.+\..+/, 'Must match an email address!'],
-	  },
-	  password: {
+		match: [/.+@.+\..+/, "Must match an email address!"],
+	},
+	password: {
 		type: String,
 		required: true,
 		minlength: 5,
-	  },
-	  posts: [
+	},
+	posts: [
 		{
-		  type: Schema.Types.ObjectId,
-		  ref: 'Post',
+			type: Schema.Types.ObjectId,
+			ref: "Post",
 		},
-	  ],
-	});
-	
-	// set up pre-save middleware to create password
-	userSchema.pre('save', async function (next) {
-	  if (this.isNew || this.isModified('password')) {
+	],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
 		const saltRounds = 10;
 		this.password = await bcrypt.hash(this.password, saltRounds);
-	  }
-	
-	  next();
-	});
-	
-	// compare the incoming password with the hashed password
-	userSchema.methods.isCorrectPassword = async function (password) {
-	  return bcrypt.compare(password, this.password);
-	};
-	
-	const User = model('User', userSchema);
-	
-	module.exports = User;
-	
+	}
+
+	next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+	return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
