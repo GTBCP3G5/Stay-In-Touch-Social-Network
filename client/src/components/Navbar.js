4453e299d2f544a1d6cfd9016228d5bcf91ef51f
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-30 07:34:24 -0400

    adjusted package.json

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,48 @@
-let navbarElement;
+import React, { useState, useEffect } from 'react';
 
 export default function Navbar () {
-    function screenSize () {
-        if(window.matchMedia("(min-width: 20px)").matches) {
-            navbarElement = <h1>Cheese</h1>
-        } else {
-            navbarElement = <h1>Gravy</h1>
-        }
-        return navbarElement;
+    const [width, setWidth] = React.useState(window.innerWidth);
+    
+    React.useEffect(() => {
+        window.addEventListener("resize", updateWidth);
+    })
+
+    const updateWidth = () => {
+        setWidth(window.innerWidth);
     }
+
+    let currentNav;
+    if(width < 500) {
+        currentNav = (
+            <div>
+            <nav className="navbar navbar-dark bg-dark">
+            <div classNAme="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            </nav>
+            <div className="collapse" id="navbarToggleExternalContent">
+                <div className="bg-dark p-4">
+                    <h5 className="text-white h4">Collapsed content</h5>
+                    <span className="text-muted">Toggleable via the navbar brand.</span>
+                </div>
+            </div>
+        </div>
+        )
+
+    } else {
+        currentNav = (
+            <nav className="">
+                <span>Favorites</span>
+                <span>Friends</span>
+            </nav>
+        )
+    }
+
     return (
-    // <div>
-    //     <nav class="navbar navbar-dark bg-dark">
-    //     <div class="container-fluid">
-    //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-    //         <span class="navbar-toggler-icon"></span>
-    //         </button>
-    //     </div>
-    //     </nav>
-    //     <div class="collapse" id="navbarToggleExternalContent">
-    //         <div class="bg-dark p-4">
-    //             <h5 class="text-white h4">Collapsed content</h5>
-    //             <span class="text-muted">Toggleable via the navbar brand.</span>
-    //         </div>
-    //     </div>
-    // </div>
     <div>
-        {screenSize()}
+        {currentNav}
     </div>
     )
 }
