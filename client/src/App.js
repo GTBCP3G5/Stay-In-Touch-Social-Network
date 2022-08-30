6541f747ad3ec093772bc5e7edfe3a8833039710
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-30 12:12:37 -0400

    Added functionality to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { InMemoryCache, ApolloClient, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home";
+import Friends from "./components/Friends";
+import Favorites from "./components/Favorites";
+import NewPost from "./components/NewPost";
+
+const client = new ApolloClient({
+	uri: '/',
+	cache: new InMemoryCache(),
+  });
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<Footer />
-		</div>
+		<ApolloProvider client={client}>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+					path="/"
+					element={<Home />}
+					/>
+					<Route
+					path="/home"
+					element={<Home />}
+					/>
+					<Route
+					path="/create_post"
+					element={<NewPost />}
+					/>
+					<Route
+					path="/friends"
+					element={<Friends />}
+					/>
+					<Route
+					path="/favorites"
+					element={<Favorites />}
+					/>
+				</Routes>
+				<Footer />
+			</Router>
+		</ApolloProvider>
 	);
 }
 
