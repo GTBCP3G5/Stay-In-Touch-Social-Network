87fbcbd100e2ca00d2ba76914f22a3d916219198
Eric Wittenstein <106685412+ericwittenstein@users.noreply.github.com>
2022-08-29 21:47:19 -0400

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -38,42 +38,6 @@ const postSchema = new Schema({
       },
     },
   ],
-	postText: {
-		type: String,
-		required: "You need to leave a post!",
-		minlength: 5,
-		maxlength: 280,
-		trim: true,
-	},
-	postAuthor: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		get: (timestamp) => dateFormat(timestamp),
-	},
-	comments: [
-		{
-			commentText: {
-				type: String,
-				required: true,
-				minlength: 1,
-				maxlength: 280,
-			},
-			commentAuthor: {
-				type: String,
-				required: true,
-			},
-			createdAt: {
-				type: Date,
-				default: Date.now,
-				get: (timestamp) => dateFormat(timestamp),
-			},
-		},
-	],
 });
 
 const Post = model("Post", postSchema);
