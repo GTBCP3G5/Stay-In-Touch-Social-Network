5246aec960d1c1d35e4f5038c299172dc35a058e
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-30 08:29:06 -0400

    Added navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,11 @@
+// Packages
 import React, { useState, useEffect } from 'react';
 
 export default function Navbar () {
+    // Set width of window to current window size
     const [width, setWidth] = React.useState(window.innerWidth);
     
+    // Resize based on current window size
     React.useEffect(() => {
         window.addEventListener("resize", updateWidth);
     })
@@ -11,12 +14,31 @@ export default function Navbar () {
         setWidth(window.innerWidth);
     }
 
+    // Value to hold Navbar
     let currentNav;
-    if(width < 500) {
+
+    // Styling
+    const navbar = {
+        backgroundColor: "#423e4f",
+        // height: "100%"
+    }
+
+    const navLinks = {
+        marginLeft: "2rem",
+        // margin: "1rem"
+    }
+
+    const navContainer = {
+        display: "flex",
+        alignItems: "center"
+    }
+
+    // For smaller screens
+    if(width < 1000) {
         currentNav = (
             <div>
-            <nav className="navbar navbar-dark bg-dark">
-            <div classNAme="container-fluid">
+            <nav style={navbar} className="navbar">
+            <div className="center container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -29,14 +51,33 @@ export default function Navbar () {
                 </div>
             </div>
         </div>
+        // <h1>Less than 1000</h1>
         )
 
-    } else {
+    } 
+    
+    // For bigger screens
+    else {
         currentNav = (
-            <nav className="">
-                <span>Favorites</span>
-                <span>Friends</span>
-            </nav>
+            <nav style={navbar} className="nav">
+        {/* <a href="/" className="site-title"></a> */}
+            <ul style={navContainer} className="left hide-on-med-and-down">
+                {/* set className to "active" to change color */}
+                <li style={navLinks} className="">
+                    <i className="material-icons">home</i>
+                </li>
+                <li style={navLinks} className="">
+                    <a href="/create_post">New Post</a>
+                </li>
+                <li style={navLinks} className="">
+                    <a href="/friends">Friends</a>
+                </li>
+                <li style={navLinks} className="">
+                    <a href="/favorites">Favorites</a>
+                </li>
+            </ul>
+        </nav>
+            // <h1>Greater than or equal to 1000</h1>
         )
     }
 
