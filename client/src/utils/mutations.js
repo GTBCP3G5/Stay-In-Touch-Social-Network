67011518ff024d8038f06ff198782fe1ec6b4079
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-31 19:36:22 -0400

    working on new post page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,56 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER =gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+export const LOGIN_USER = gql`
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const ADD_POST = gql`
+	mutation addPost($postText: String!) {
+		addPost(postText: $postText) {
+			_id
+			postText
+			postAuthor
+			createdAt
+			comments {
+				_id
+				commentText
+			}
+		}
+	}
+`;
+
+export const ADD_COMMENT = gql`
+	mutation addComment($postId: ID!, $commentText: String!) {
+		addComment(postId: $postId, commentText: $commentText) {
+			_id
+			postText
+			postAuthor
+			createdAt
+			comments {
+				_id
+				commentText
+				createdAt
+			}
+		}
+	}
 `;
