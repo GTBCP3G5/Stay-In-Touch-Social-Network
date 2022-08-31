281ea6eb9454a10b1e4ddca0dec0d8acf68d47bc
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-31 19:44:51 -0400

    staging changes

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -6,6 +6,20 @@ export default function PostList({ posts, title }) {
     return (
         <div>
             <h2>{title}</h2>
+            {posts &&
+            posts.map((post) => (
+                <div key={post._id} className="card mb-3">
+                    <h4 className="p-2 m-0">
+                        {post.postAuthor} <br />
+                        <span style={{ fontSize: '1rem' }}>
+                            added this post on {post.createdAt}
+                        </span>
+                    </h4>
+                    <div>
+                        <p>{post.postText}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,12 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_POSTS = gql`
+    query getPosts {
+        posts {
+           _id
+           postText
+           postAuthor
+           createdAt 
+        }
+    }
+`
