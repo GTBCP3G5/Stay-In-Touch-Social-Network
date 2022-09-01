d3756f235c86d8c1cb8a9532f249934e824652f1
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-01 17:52:00 -0400

    deployment testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Named the database 'bootSocial'...we can change that if you guys want
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bootSocialDB",
+	process.env.MONGODB_URI || "mongodb://localhost:27017/bootSocialDB",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
