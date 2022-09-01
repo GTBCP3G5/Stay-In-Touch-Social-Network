2a1012cd64743b57acb299272a4ddae8df3fcc00
Josh Hawk <hawkjosh@icloud.com>
2022-08-31 23:53:12 -0400

    registration component working now

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
     github: ''
   });
 
-  const [register, { error, data }] = useMutation(ADD_USER);
+  const [register, { error }] = useMutation(ADD_USER);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -30,7 +30,7 @@ export default function Register() {
         variables: { ...formData },
       });
 
-      Auth.register(data.register.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
     }
@@ -47,7 +47,7 @@ export default function Register() {
     <figure className="h-screen flex bg-gray-600">
       <img className="object-scale-down" src="logo-2.png" alt="SIT Logo" />
 
-      <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-1">
+      <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default px-1">
         <div className="text-primary m-6">
           <div className="flex items-center mt-3 justify-center">
             <h1 className="text-2xl font-medium text-primary mt-4 mb-2">
@@ -61,16 +61,16 @@ export default function Register() {
 
           <form onSubmit={handleSubmit}>
             <label>Username:</label>
-            <input name="username" type="text" value={formData.username} onChange={handleChange} placeholder="Username" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+            <input name="username" type="text" value={formData.username} onChange={handleChange} placeholder="Username" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-2"} />
 
             <label>Email:</label>
-            <input name="email" type="text" value={formData.email} onChange={handleChange} placeholder="Email" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+            <input name="email" type="text" value={formData.email} onChange={handleChange} placeholder="Email" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-2"} />
 
             <label>Password:</label>
-            <input name="password" type="password" value={formData.password} onChange={handleChange} placeholder="Password" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+            <input name="password" type="password" value={formData.password} onChange={handleChange} placeholder="Password" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-2"} />
 
             <label>GitHub:</label>
-            <input name="github" type="text" value={formData.github} onChange={handleChange} placeholder="GitHub" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+            <input name="github" type="text" value={formData.github} onChange={handleChange} placeholder="GitHub (optional)" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-2"} />
 
             <div className="flex items-center mt-3 justify-center">
               <button type="submit" style={{ cursor: "pointer" }} className={"bg-blue-700 hover:bg-blue-500 py-2 px-4 text-md text-white rounded border border-blue focus:outline-none focus:border-black"}>Register</button>
