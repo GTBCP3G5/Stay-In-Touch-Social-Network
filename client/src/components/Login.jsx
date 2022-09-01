5cef513d9b4aa29d0a3d00f8b4f8b53cd4604698
Josh Hawk <hawkjosh@icloud.com>
2022-08-31 21:24:59 -0400

    more work on login/registration functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,30 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 export default function Login() {
   const [formData, setFormData] = useState({ 
-    email: '', 
-    password: ''
+    username: '',
+    email: ''
   });
 
-  const [showAlert, setShowAlert] = useState(false);
-
-  const [login, { error }] = useMutation(LOGIN_USER);
-
-  useEffect(() => {
-    if (error) {
-      setShowAlert(true);
-    } else {
-      setShowAlert(false);
-    }
-  }, [error]);
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
-      ...formData.user,
+      ...formData,
       [name]: value
     });
   };
@@ -32,81 +23,59 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
     try {
       const { data } = await login({
         variables: { ...formData },
       });
 
-      console.log(data);
       Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
     }
 
     setFormData({
-      email: '',
-      password: '',
+      username: '',
+      password: ''
     });
   };
 
   return (
     <figure className="h-screen flex bg-gray-600">
       <img className="object-scale-down" src="logo-2.png" alt="SIT Logo" />
+
       <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-1">
-        <blockquote className="text-2xl font-medium text-center">
-          <p className="text-lg font-semibold">Stay In Touch!</p>
-        </blockquote>
-        
         <div className="text-primary m-6">
           <div className="flex items-center mt-3 justify-center">
             <h1 className="text-2xl font-medium text-primary mt-4 mb-2">
               Login to your account
             </h1>
           </div>
-          <form onSubmit={handleSubmit}>
 
+          {Auth.loggedIn() ? (<Navigate to='/home' />)
+
+          : (
+
+          <form onSubmit={handleSubmit}>
             <label className="text-left">Username:</label>
-            <input
-              name="username"
-              type="text"
-              value={formData.user.username}
-              onChange={handleChange}
-              placeholder="Username"
-              className={
-                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
-              }
-            />
+            <input name="username" type="text" value={formData.username} onChange={handleChange} placeholder="Username" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+
             <label>Password:</label>
-            <input
-              name="password"
-              type="password"
-              value={formData.user.password}
-              onChange={handleChange}
-              placeholder="Password"
-              className={
-                "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
-              }
-            />
+            <input name="password" type="password" value={formData.password} onChange={handleChange} placeholder="Password" className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"} />
+
             <div className="flex items-center mt-3 justify-center">
-              <button
-                className={
-                  "bg-blue-700 hover:bg-blue-500 py-2 px-4 text-md text-white rounded border border-blue focus:outline-none focus:border-black"
-                }
-                value="Login"
-              >
-                Login
-              </button>
+              <button type="submit" style={{ cursor: "pointer" }} className={"bg-blue-700 hover:bg-blue-500 py-2 px-4 text-md text-white rounded border border-blue focus:outline-none focus:border-black"}>Login</button>
             </div>
           </form>
+
+          )}
+
+          {error && (
+            <div className="text-xl font-bold text-red-700 uppercase mt-6 mb-2">{'Incorrect username or password'}</div>
+          )}
+
           <div className="flex items-center mt-3 justify-center">
           <button className={"justify-center text-blue-500 hover:underline"}>
-            Need to register?
+            <Link to="/register">Need to register?</Link>
           </button>
           </div>
         </div>
