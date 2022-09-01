65957900780b19ce6c3c669e0a714104fb22fff5
KimboSlice <kimberlyhoward529@gmail.com>
2022-09-01 18:14:27 -0400

    Added 'add friend' button to homepage and changed color of buttons once clicked

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -31,6 +31,9 @@ export default function PostList({ posts, title }) {
                     <div className="m-3 text-zinc-800">
                         <p style={normalKanit}>{post.postText}</p>
                     </div>
+                    <div class="flex space-x-2 m-2">
+                        <button type="button" class="inline-block px-6 py-2.5 bg-sky-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-400 hover:shadow-lg focus:bg-sky-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Add Friend</button>
+                    </div>
                 </div>
             ))}
         </div>
