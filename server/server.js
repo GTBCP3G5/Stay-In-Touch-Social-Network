184d3f42bccbdddfedaf81d9b0e6078318d18104
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-02 02:54:21 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -23,8 +23,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "/public/index.html"));
-	app.use(express.static(path.join(__dirname, "/public/index.html")));
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
