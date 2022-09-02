cc055f0ec8b47b6edf645dad0759773645d726fb
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-02 01:29:58 -0400

    testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 // Named the database 'bootSocial'...we can change that if you guys want
 mongoose.connect(
-	process.env.MONGODB_URI ||
-		"mongodb+srv://ericwittenstein:2SnHfy%40QK!SB8e4@cluster0.jpjowxc.mongodb.net/bootSocialDB?retryWrites=true&w=majority" ||
-		"mongodb://localhost:27017/bootSocialDB",
+	process.env.MONGODB_URI || "mongodb+srv://ericwittenstein:2SnHfy%40QK!SB8e4@cluster0.jpjowxc.mongodb.net/bootSocialDB?retryWrites=true&w=majority",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
