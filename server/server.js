b0f554c6ae0ecbd4a4ca61382d5072911b703c98
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-02 02:48:30 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -19,12 +19,12 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build/index.html")));
+	app.use(express.static(path.join(__dirname, "/public/index.html")));
 }
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/build/index.html"));
-	app.use(express.static(path.join(__dirname, "../client/build/index.html")));
+	res.sendFile(path.join(__dirname, "/public/index.html"));
+	app.use(express.static(path.join(__dirname, "/public/index.html")));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
