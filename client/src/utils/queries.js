d23d69b2d08a66a4cda98d6dd02f7b515c3f7685
Josh Hawk <hawkjosh@icloud.com>
2022-09-01 23:39:27 -0400

    added users list functionality

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,13 @@
 import { gql } from "@apollo/client";
 
+export const QUERY_USERS = gql`
+  query Users {
+    users {
+      username
+    }
+  }
+`;
+
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
