266018c983c91b5b84a6c5211f52d6818c1809c8
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-01 23:24:58 -0400

    deployment

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
+require("dotenv").config();
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = new ApolloServer({
 	typeDefs,
@@ -23,8 +26,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
-	app.use(express.static(path.join(__dirname, "../client/build")));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 })
 
 app.get("/", (req, res) => {
